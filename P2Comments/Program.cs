c52b1d06c81abfc302c334ad8976a6700f89d05c
jackahlberg <ahlberg.jack@gmail.com>
2022-01-25 12:51:32 +0100

    Creates P2Comments. Writes all code for assignment.

---
+++ P2Comments/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+// Jack Fors Ahlberg
+// Jan 25th, 2022
+// jack.fors.ahlberg@student.forsbergsskola.se
+
+namespace Comments
+{
+    class Program
+    {
+        /// <summary>
+        /// A main method is called whenever the applications starts.
+        /// </summary>
+        static void Main()
+        {
+            //This method will print the text Hello World! to the console.
+            Console.WriteLine("Hello World!");
+            
+        }
+        
+    }
+}
