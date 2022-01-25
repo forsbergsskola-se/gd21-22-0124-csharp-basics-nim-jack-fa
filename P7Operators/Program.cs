1236631048ce4164f602e2a079f1079bdcda773f
jackahlberg <ahlberg.jack@gmail.com>
2022-01-25 16:42:54 +0100

    Writes all code for P7Operators

--- P7Operators/Program.cs
+++ P7Operators/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+
+    static void Main()
+    {
+        
+        Console.WriteLine(((7 + 5) * 3 - 6) / 1.2 % 17);
+
+    }
+    
+}
