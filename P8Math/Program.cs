19ff0ae1fe514ebcafce1b65cb25f4249fdee27a
jackahlberg <ahlberg.jack@gmail.com>
2022-01-25 12:20:54 +0100

    Starts P8Math code.
    Misunderstanding of assignment order. Will work on P2Comment now instead.

--- P8Math/Program.cs
+++ P8Math/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace P8Math
+{
+    class Maths
+    {
+        private string first;
+        private string second;
+        static void Main()
+        {
+            Console.WriteLine("Give me your first number");
+            string first = Console.ReadLine();
+            Console.WriteLine("Give me your second number");
+            string second = Console.ReadLine();
+            
+
+        }
+    }
+}
