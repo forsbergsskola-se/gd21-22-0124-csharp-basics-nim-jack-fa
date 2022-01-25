5e647d404abc9eed2996f5e3a3fea430b8b6a278
jackahlberg <ahlberg.jack@gmail.com>
2022-01-25 16:05:44 +0100

    Writes all code for P5Conversion

--- P5Conversion/Program.cs
+++ P5Conversion/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+
+    static void Main()
+    {
+
+        int number = 5;
+        float convertNumber;
+        string numberstring = number.ToString();
+        
+        Console.WriteLine(numberstring);
+        
+        numberstring = "56.3";
+        convertNumber = Convert.ToSingle(numberstring);
+        number = Convert.ToInt32(convertNumber);
+        Console.WriteLine(number);
+
+    }
+    
+}
