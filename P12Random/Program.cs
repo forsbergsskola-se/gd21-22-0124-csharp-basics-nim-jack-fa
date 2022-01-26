83fbb4bb0ece0c0cb0f64af8419f31ec8e82e4ad
jackahlberg <ahlberg.jack@gmail.com>
2022-01-26 15:02:36 +0100

    Writes all code for P12Random

--- P12Random/Program.cs
+++ P12Random/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+
+    static void Main()
+    {
+
+        
+        int seed;
+        
+        Console.WriteLine("Please enter a seed");
+        seed = Convert.ToInt32(Console.ReadLine());
+        Random random = new Random(seed);
+
+        int num1;
+        int num2;
+        int num3;
+
+        num1 = random.Next(0, 6);
+        num2 = random.Next(0, 6);
+        num3 = random.Next(0, 6);
+        
+        Console.WriteLine("Three integers between 0-5:");
+        Console.WriteLine(num1);
+        Console.WriteLine(num2);
+        Console.WriteLine(num3);
+
+
+        double num4 = random.NextDouble() / 2;
+        double num5 = random.NextDouble() / 2;
+        double num6 = random.NextDouble() / 2;
+
+        Console.WriteLine("Three numbers between 0.0 and 0.5:");
+        Console.WriteLine(num4);
+        Console.WriteLine(num5);
+        Console.WriteLine(num6);
+
+        double num7 = random.NextDouble() * ((0.7 - 0.2) + 0.2);
+        double num8 = random.NextDouble() * ((0.7 - 0.2) + 0.2);
+        double num9 = random.NextDouble() * ((0.7 - 0.2) + 0.2);
+        
+        Console.WriteLine("Three numbers between 0.2 and 0.7");
+        Console.WriteLine(num7);
+        Console.WriteLine(num8);
+        Console.WriteLine(num9);
+
+    }
+    
+}
