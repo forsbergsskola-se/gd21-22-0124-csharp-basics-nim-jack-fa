1414370ecb8646a0a4c3286380803157f3b99d15
jackahlberg <ahlberg.jack@gmail.com>
2022-01-26 11:42:08 +0100

    Writes all code for P10Boolean

--- P10Boolean/Program.cs
+++ P10Boolean/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+
+    static void Main()
+    {
+
+        string ageString;
+        int age;
+        
+        Console.WriteLine("What is your age?");
+        ageString = Console.ReadLine();
+        age = Convert.ToInt32(ageString);
+        
+        bool isChild = age <= 12;
+        bool isTeenager = 13 <= age & age <= 19;
+        bool isGrownup = age > 19;
+        
+        Console.WriteLine("You are a child: " + isChild);
+        Console.WriteLine("You are a teenager: " + isTeenager);
+        Console.WriteLine("You are a grown-up: " + isGrownup);
+
+    }
+    
+}
